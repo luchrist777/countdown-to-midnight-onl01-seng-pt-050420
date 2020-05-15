8ce970812ed3666da382b6343b41e313489c1323
Luis Leandro <luchrist777@gmail.com>
2020-05-15 17:09:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,26 +1,10 @@
 def countdown(number)
   while number > 0
-    puts "#{number} SECONDS(S)!"
-    number -= 1
-  end
- "HAPPY NEW YEAR!"
-end
-
-
-
-
-
-
-
-
-
-
-def countdown(number)
-  while number > 0
    puts "#{number} SECOND(S)!"
    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep
+def countdown_with_sleep
+end
